1da234585f97bf9d57c8486d48433b9740d6e07e
PrasanKumar <prasankumar93@gmail.com>
2024-05-30 18:29:04 +0530

    socket old and new code to debug with main

--- streaming-llm-output/src/socket.ts
+++ streaming-llm-output/src/socket.ts
@@ -1,5 +1,4 @@
-import type { IMessageHandler } from "./utils/redis-wrapper";
-
+// socket.ts is with "xRead" , socket-new.ts is with "xReadGroup"
 import { v4 as uuidv4 } from "uuid";
 
 import { Server } from "socket.io";
@@ -10,30 +9,6 @@ import { LoggerCls } from "./utils/logger.js";
 import { askQuestion } from "./question.js";
 import { CONFIG } from "./config.js";
 
-const listenStream = (
-  socket: any,
-  activeListeners: Map<string, boolean>,
-  userId: string,
-  messageCallback: IMessageHandler
-) => {
-  // listen to redis stream for new messages and pass it to messageCallback
-  redisUtils.listenToStreamsByReadGroup(
-    {
-      streams: [
-        {
-          streamKeyName: `${CONFIG.OPENAI_STREAM}:${userId}`,
-          messageCallback: messageCallback,
-        },
-      ],
-      groupName: `${CONFIG.OPENAI_STREAM}_Grp_${userId}`,
-      consumerName: `${CONFIG.OPENAI_STREAM}_Con_${userId}`,
-      maxNoOfEntriesToReadAtTime: 1,
-    },
-    socket.id,
-    activeListeners
-  );
-};
-
 const initSocket = async (socketServer: Server, model: ChatOpenAI) => {
   const activeListeners = new Map<string, boolean>();
 
@@ -41,17 +16,37 @@ const initSocket = async (socketServer: Server, model: ChatOpenAI) => {
     LoggerCls.info("a user connected");
     activeListeners.set(socket.id, true);
 
-    const loginUserId = "USER1"; //example user id
-    listenStream(socket, activeListeners, loginUserId, (message, messageId) => {
-      LoggerCls.info("-: " + message.chunkOutput);
-      socket.emit("chunk", message.chunkOutput); // Emit chunk to client (browser)
-    });
-
     socket.on("askQuestion", async ({ topic, topicQuestion }) => {
-      activeListeners.set(socket.id, true);
-      const questionId = "Q-" + uuidv4();
-      const streamName = `${CONFIG.OPENAI_STREAM}:${loginUserId}`;
-      askQuestion(model, questionId, topic, topicQuestion, streamName); //trigger async
+      const questionId = uuidv4();
+
+      //lastId to prevent re scan of prev question (or use consumer groups)
+      const lastId = await redisUtils.getLastIdOfStream(CONFIG.OPENAI_STREAM);
+      LoggerCls.debug("lastId", lastId);
+      //trigger the question async
+      askQuestion(
+        model,
+        questionId,
+        topic,
+        topicQuestion,
+        CONFIG.OPENAI_STREAM
+      );
+
+      // Listen for new messages on the Redis stream
+      const startChunk = `START:${questionId};`;
+      const endChunk = `;END:${questionId}`;
+      redisUtils.readStream(
+        CONFIG.OPENAI_STREAM,
+        lastId,
+        startChunk,
+        endChunk,
+        socket.id,
+        activeListeners,
+        (data) => {
+          LoggerCls.info(data.chunkOutput);
+          // Emit the chunk to the client (browser)
+          socket.emit("chunk", data.chunkOutput);
+        }
+      );
     });
 
     socket.on("disconnect", () => {
